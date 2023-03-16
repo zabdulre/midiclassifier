bd88e4acfd2ce6c646685468989bba739f181440
Zaid Abdulrehman <zabdulre@usc.edu>
2023-03-16 16:01:08 -0700

    Succesfully read MIDI file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import music21
 
 # read the files in to midi objects
 # send the object into a processing function
@@ -7,14 +8,19 @@ import os
 # get the vector from the processing function, and put the correct label
 # pass it to sci kit learn
 
-Labels = {"romantic": [1, 0, 0, 0], "baroque": [0, 1, 0, 0], "classical": [0, 0, 1, 0], "modern": [0, 0, 0, 1]}
+Labels = {"romantic": 1, "baroque": 2, "classical": 3, "modern": 4}
 
 
-def getFeatures(file):
+def getFeatures(filePath):
     # use music21 to open the file
+    file = music21.midi.MidiFile()
+    file.open(filePath)
+    file.read()
+    file.close()
+    print(file.tracks)
     # get a vector for each feature we want to add
     # append those vectors together into one giant vector
-    return
+    return []
 
 
 def getClassDirectories(argv):
@@ -30,17 +36,19 @@ def main(argv):
     directories = getClassDirectories(argv)
 
     loadedData = []  # List of examples (vectors) for each era of music, each example is a dictionary {Label : feature vector}
-    for directoryEntry in directories:
-        currentLabel = Labels[directoryEntry[0]]
-        for fileName in os.listdir(directoryEntry[1]):
-            loadedData.append({currentLabel: getFeatures(fileName)})
+    for folder in directories.items():
+        currentLabel = Labels[folder[0]]
+        for fileName in os.listdir(folder[1]):
+            currentFileDirectory = folder[1] + "/" + fileName
+            currentFeatures = getFeatures(currentFileDirectory)
+            loadedData.append({currentLabel: currentFeatures})
 
     return
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument("--datadir", type=str, help="directory to folders of midi files")
+    p.add_argument("--datadir", type=str, help="directory to folders of midi files", required=True)
     p.add_argument("--romantic", default="romantic", type=str, help="name of folder of romantic era midi files")
     p.add_argument("--baroque", default="baroque", type=str, help="name of folder of baroque era midi files")
     p.add_argument("--classical", default="classical", type=str, help="name of folder of classical era midi files")
